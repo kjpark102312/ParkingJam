6c89c21ec4300251b9a57b6ad81b6386026bec0e
DESKTOP-DTNOCP5\user <kjpark102312@naver.com>
2022-11-08 17:58:28 +0900

    스테이지 수정 및 Pass 기능 연출 작업 및 코드 수정

--- ParkingJamProject/Assets/01.Scripts/Car.cs
+++ ParkingJamProject/Assets/01.Scripts/Car.cs
@@ -1,7 +1,9 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class Car : MonoBehaviour
 {
@@ -11,31 +13,48 @@ public class Car : MonoBehaviour
 
     public bool isMove = false;
 
-
     float speed = 15f;
 
     public Transform passPos;
 
-    NavMeshAgent agent;
-
     Vector3 curMoveDir;
 
+    public Transform[] corners;
+
+    public Transform targetCorner;
+
+
+    int cornerIndex = 0;
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
-        agent = GetComponent<NavMeshAgent>();
-
-        agent.updateRotation = false;
-        agent.updateUpAxis = false;
     }
 
     private void Update()
     {
-
-
         if (!isMove)
         {
-            rb.constraints = RigidbodyConstraints.FreezeRotation;
+            rb.constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ;
+        }
+
+        for (int i = 0; i < corners.Length; i++)
+        {
+            if (Mathf.Round(transform.position.x) == Mathf.Round(corners[i].position.x) || Mathf.Round(transform.position.y) == Mathf.Round(corners[i].position.y))
+            {
+                Debug.Log("Stop");
+                StopAllCoroutines();
+
+                rb.velocity = Vector3.zero;
+
+                transform.DORotate(new Vector3(0f, -90f, 0), 0.2f).OnComplete(() =>
+                {
+                    if (CheckAngle(targetCorner.position - transform.position))
+                        return;
+                    else
+                        targetCorner = corners[cornerIndex + 1];
+                });
+
+            }
         }
     }
 
@@ -74,12 +93,20 @@ public class Car : MonoBehaviour
     {
         RaycastHit hit;
 
-
         Debug.DrawRay(transform.position, dir, Color.red, 20f);
-        if (Physics.Raycast(transform.position, dir, out hit, 100f, 1 << 6))
-        {
-            Debug.Log(hit);
+        Debug.DrawRay(transform.position + new Vector3(0, 0, -1f), dir, Color.red, 20f);
+        Debug.DrawRay(transform.position + new Vector3(0, 0, +1f), dir, Color.red, 20f);
 
+        if (Physics.Raycast(transform.position + new Vector3(0, 0, -1f), dir, out hit, 20f, 1 << 6))
+        {
+            return;
+        }
+        if (Physics.Raycast(transform.position + new Vector3(0, 0, +1f), dir, out hit, 20f, 1 << 6))
+        {
+            return;
+        }
+        if (Physics.Raycast(transform.position, dir, out hit, 20f, 1 << 6))
+        {
             return;
         }
 
@@ -94,10 +121,16 @@ public class Car : MonoBehaviour
         rb.constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionY;
 
         PassCheck(dir);
+        GetTargetCorner();
 
         while (true)
         {
             rb.velocity = dir * speed;
+
+            if (targetCorner != null)
+            {
+                
+            }
 
             yield return null;
         }
@@ -105,47 +138,78 @@ public class Car : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        
-
         if (collision.gameObject.CompareTag("Car") || collision.gameObject.CompareTag("Wall"))
         {
             StopAllCoroutines();
 
             ColKnockBack();
-    
-            
-            
-            rb.constraints = RigidbodyConstraints.FreezeRotation;
 
-            
+            rb.constraints = RigidbodyConstraints.FreezeRotation;
         }
     }
 
+    void GetTargetCorner()
+    {
+        targetCorner = corners[0];
+
+        for (int i = 0; i < corners.Length; i++)
+        {
+            if (Vector3.Distance(transform.position, targetCorner.position) > Vector3.Distance(transform.position, corners[i].position))
+            {
+                targetCorner = corners[i];
+                cornerIndex = i;
+
+                Debug.Log("¾Æ¾Ç");
+            }
+        }
+    }
+
+
+    bool CheckAngle(Vector3 dir)
+    {
+        float dot = Vector3.Dot(dir.normalized, -transform.right.normalized);
+
+        float theta = Mathf.Acos(dot) * (180 / Mathf.PI);
+
+        if(theta <= 90)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+
     void ColKnockBack()
     {
-        if(isMove)
+        if (isMove)
         {
             Debug.Log("³Ë¹é");
 
             rb.velocity = Vector3.zero;
             rb.constraints = RigidbodyConstraints.FreezeRotation;
 
-            rb.AddForce(-curMoveDir * 3f, ForceMode.Impulse);
+            rb.AddForce(-curMoveDir * 2.5f, ForceMode.Impulse);
 
-            Debug.Log(-curMoveDir * 3f);
-            isMove = false;
+            Invoke("FreezePos", 0.3f);
         }
     }
 
+
+
     public void Pass()
     {
-        //  ÆÐ½º
-        StopAllCoroutines();
-        rb.velocity = Vector3.zero;
-        agent.destination = passPos.position;
+        this.gameObject.layer = 0;
 
-       
+
         Debug.Log("ÆÐ½º");
+    }
+
+    void FreezePos()
+    {
+        isMove = false;
     }
 }
 
