ea3059bdd2012f27e65269003a8efa336d163f41
DESKTOP-DTNOCP5\user <kjpark102312@naver.com>
2022-11-11 17:59:38 +0900

    파일 정리 및 PatrolAI 수정

--- ParkingJamProject/Assets/01.Scripts/Obstacle/People.cs
+++ ParkingJamProject/Assets/01.Scripts/Obstacle/People.cs
@@ -50,6 +50,8 @@ public class People : MonoBehaviour
     {
         if(collision.gameObject.CompareTag("Car"))
         {
+            agent.isStopped = true;
+
             uIManager.GameOverTween();
         }
     }
