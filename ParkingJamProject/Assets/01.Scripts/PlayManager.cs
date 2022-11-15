ccd94f657f286082b2c20809f71cc4324a68a69b
DESKTOP-DTNOCP5\user <kjpark102312@naver.com>
2022-11-15 17:50:20 +0900

    한 사이클을 위한 작업 PlayManager 작성

---
+++ ParkingJamProject/Assets/01.Scripts/PlayManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayManager : MonoBehaviour
+{
+    Car[] cars;
+    Goal goal;
+    void Start()
+    {
+        cars = FindObjectsOfType<Car>();
+        goal = FindObjectOfType<Goal>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void CheckClear()
+    {
+        int count = 0;
+
+        for (int i = 0; i < cars.Length; i++)
+        {
+            if (!cars[i].gameObject.activeSelf)
+            {
+                count++;
+            }
+        }
+
+        if (count == cars.Length)
+        {
+            Debug.Log("Clear");
+
+            
+        }
+    }
+}
